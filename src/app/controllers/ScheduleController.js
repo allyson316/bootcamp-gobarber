13f2b54fb2344e179c5f6632e8cdbc76b9762c8b
Allyson Andrade <allyson316@gmail.com>
2019-07-06 14:44:02 -0300

    feat: listando agenda do prestador

---
+++ src/app/controllers/ScheduleController.js
@@ -0,0 +1,33 @@
+import { startOfDay, endOfDay, parseISO } from 'date-fns';
+import { Op } from 'sequelize'; // operador do sequelize
+import Appointment from '../models/Appointment';
+import User from '../models/User';
+
+class ScheduleController {
+  async index(req, res) {
+    const checkUserProvider = await User.findOne({
+      where: { id: req.userId, provider: true },
+    });
+
+    if (!checkUserProvider) {
+      return res.status(401).json({ error: 'Usuário não é um prestador!' });
+    }
+
+    const { date } = req.query;
+    const parseDate = parseISO(date);
+
+    const appointments = await Appointment.findAll({
+      where: {
+        provider_id: req.userId,
+        canceled_at: null,
+        // listar agendamentos que estão entre o começo e o fim do date passado
+        date: { [Op.between]: [startOfDay(parseDate), endOfDay(parseDate)] },
+      },
+      order: ['date'],
+    });
+
+    return res.json({ appointments });
+  }
+}
+
+export default new ScheduleController();
--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
@@ -21,6 +22,8 @@ routes.put('/users', UserController.update);
 routes.get('/providers', ProviderController.index);
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
+
+routes.get('/schedule', ScheduleController.index);
 
 // rota responsável por salvar um arquivo
 routes.post('/files', upload.single('file'), FileController.store);
