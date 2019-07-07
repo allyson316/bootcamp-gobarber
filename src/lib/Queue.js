9847e37f0d3a4adb1ce242c9952817dd0a5afdbe
Allyson Andrade <allyson316@gmail.com>
2019-07-07 16:31:53 -0300

    feat: monitorando falhas na fila

--- src/lib/Queue.js
+++ src/lib/Queue.js
@@ -38,8 +38,12 @@ class Queue {
        * handle =  tarefa a ser processada na fila
        */
       const { bee, handle } = this.queues[job.key];
-      bee.process(handle);
+      bee.on('failed', this.handleFailure()).process(handle);
     });
+  }
+
+  handleFailure(job, err) {
+    console.log(`Queue ${job.queue.name}: FAILED`, err);
   }
 }
 export default new Queue();
